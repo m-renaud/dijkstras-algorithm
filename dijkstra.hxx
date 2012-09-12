f751378ee0ae6ad7f597f56e1e1c1b5f41464167
Matt Renaud <mrenaud92@gmail.com>
2012-09-11 23:02:55 -0400

    Added operator to calculate distances and ability to set different init values.
    find_path() now takes:
      Two template paramaters:
        Pred: A predicate for comparing the current distance with a new distance
        Op:   A binary operator to set a new distance given an old distance
          	  and an edge weight
      An additional argument:
        init_val: The value to initialize all nodes with at the onset

--- dijkstra.hxx
+++ dijkstra.hxx
@@ -2,6 +2,7 @@
 #define MRR_DIJKSTRA_HXX_
 
 #include <algorithm>
+#include <functional>
 #include <limits>
 #include <string>
 #include <utility>
@@ -16,9 +17,11 @@ namespace mrr {
 
 
 //m=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-template <typename weight_type>
+template <typename WeightType, typename LabelType>
 struct dijkstra_vertex
 {
+  using weight_type = WeightType;
+  using label_type = LabelType;
   using index_type = std::size_t;
   using neighbour_type = std::pair<weight_type, index_type>;
   using neighbour_list_type = std::vector<neighbour_type>;
@@ -41,12 +44,12 @@ struct dijkstra_vertex
 
 
 //m=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-template <typename weight_type, typename Comparator>
+template <typename WeightType, typename LabelType, typename Comparator>
 struct dijkstra_weight_compare
 {
   bool operator()(
-    dijkstra_vertex<weight_type>* a,
-    dijkstra_vertex<weight_type>* b
+    dijkstra_vertex<WeightType,LabelType>* a,
+    dijkstra_vertex<WeightType,LabelType>* b
   )
   {
     return Comparator()(b->distance, a->distance);
@@ -56,11 +59,12 @@ struct dijkstra_weight_compare
 
 
 //m=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-template <typename WeightType>
+template <typename WeightType, typename LabelType = std::size_t>
 class dijkstra
 {
   using weight_type = WeightType;
-  using vertex_type = dijkstra_vertex<weight_type>;
+  using label_type = LabelType;
+  using vertex_type = dijkstra_vertex<weight_type, label_type>;
   using index_type = typename vertex_type::index_type;
   using vertex_list_type = std::vector<vertex_type>;
   using size_type = typename vertex_list_type::size_type;
@@ -90,25 +94,45 @@ public:
     ++graph_size;
   }
 
-  weight_type find_shortest_path(index_type start, index_type destination)
+  template <
+    typename Pred = std::less<weight_type>,
+    typename Op = std::plus<weight_type>
+  >
+  weight_type find_path(
+    index_type start, index_type destination,
+    weight_type init_val = std::numeric_limits<weight_type>::max()
+  )
   {
-    vertex_distance_init_value = std::numeric_limits<weight_type>::max();
+    vertex_distance_init_value = init_val;
     reset_graph();
-    return find_path<std::less<weight_type> >(start, destination);
+    return find_path_impl<Pred,Op>(start, destination);
   }
 
-  weight_type find_longest_path(index_type start, index_type destination)
+  weight_type find_shortest_path(
+    index_type start, index_type destination,
+    weight_type init_val = std::numeric_limits<weight_type>::max()
+  )
   {
-    vertex_distance_init_value = std::numeric_limits<weight_type>::min();
+    vertex_distance_init_value = init_val;
     reset_graph();
-    return find_path<std::greater<weight_type> >(start, destination);
+    return find_path_impl<std::less<weight_type> >(start, destination);
+  }
+
+  weight_type find_longest_path(
+    index_type start, index_type destination,
+    weight_type init_val = std::numeric_limits<weight_type>::min()
+  )
+  {
+    vertex_distance_init_value = init_val;
+    reset_graph();
+    return find_path_impl<std::greater<weight_type> >(start, destination);
   }
 
 private:
   void reset_graph();
 
-  template <typename Comparator>
-  weight_type find_path(index_type start, index_type destination);
+  template <typename Pred, typename Op = std::plus<weight_type> >
+  weight_type find_path_impl(index_type start, index_type destination);
 
   // Graph representation
   std::vector<vertex_type> vertices;
@@ -124,8 +148,8 @@ private:
 
 
 //m=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-template <typename WeightType>
-void dijkstra<WeightType>::reset_graph()
+template <typename WeightType, typename LabelType>
+void dijkstra<WeightType,LabelType>::reset_graph()
 {
   for(std::size_t i = 0; i < graph_size; ++i)
   {
@@ -136,15 +160,16 @@ void dijkstra<WeightType>::reset_graph()
 
 
 //m=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-template <typename WeightType>
-template <typename Comparator>
-WeightType dijkstra<WeightType>::find_path(
+template <typename WeightType, typename LabelType>
+template <typename Pred, typename Op>
+WeightType dijkstra<WeightType,LabelType>::find_path_impl(
   index_type start, index_type destination
 )
 {
   vertex_type* current;
   std::vector<vertex_type*> unvisited_nodes(graph_size);
-  Comparator comp;
+  Pred comp;
+  Op op;
 
   for(std::size_t i = 0; i < graph_size; ++i)
     unvisited_nodes[i] = &vertices[i];
@@ -154,7 +179,7 @@ WeightType dijkstra<WeightType>::find_path(
   std::make_heap(
     unvisited_nodes.begin(),
     unvisited_nodes.end(),
-    dijkstra_weight_compare<weight_type,Comparator>()
+    dijkstra_weight_compare<weight_type,label_type,Pred>()
   );
 
 
@@ -169,12 +194,12 @@ WeightType dijkstra<WeightType>::find_path(
        || current->index == destination)
       break;
 
-    // Neighbour :: std::pair<weight_type, node_type>
+    // Neighbour :: std::pair<weight_type, vertex_type>
     for(neighbour_type& neighbour : current->neighbours)
     {
       if(!vertices[neighbour.second].visited)
       {
-        weight_type alternate_route = current->distance + neighbour.first;
+        weight_type alternate_route = op(current->distance,neighbour.first);
 
         if(comp(alternate_route, vertices[neighbour.second].distance))
           vertices[neighbour.second].distance = alternate_route;
@@ -184,7 +209,7 @@ WeightType dijkstra<WeightType>::find_path(
     std::make_heap(
       unvisited_nodes.begin(),
       unvisited_nodes.end(),
-      dijkstra_weight_compare<weight_type, Comparator>()
+      dijkstra_weight_compare<weight_type,label_type,Pred>()
     );
 
   } // while(nodes to visit)
